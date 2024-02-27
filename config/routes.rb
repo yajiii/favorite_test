8136d9aa11d344019cefc6b1c21270b7a0f9f05b
yajima kumiko <yajimakumiko@yajimanoMacBook-Pro.local>
2024-02-27 17:54:34 +0900

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :products
-  resources :shops
+  root to:  'shops#index'
+  resources :products, expect: [:show] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :shops, expect: [:index, :show] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :favorites, only: [:index]
 end
