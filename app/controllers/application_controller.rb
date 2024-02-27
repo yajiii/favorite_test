20ef5befc33462808f16d3329e138587854dbf15
yajima kumiko <yajimakumiko@yajimanoMacBook-Pro.local>
2024-02-27 17:54:08 +0900

    認証関係の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+  def after_sign_in_path_for(resource)
+    shops_path(resource)
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    request.referer
+  end
 end
